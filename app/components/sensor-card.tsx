37e8daa5ecedd6c1f3c8942bd0a630c6a1ecc91f
art millen lisboa <art@sugartech.ph>
2025-12-16 13:04:18 +0800

    feat: add component for sensor cards

--- app/components/sensor-card.tsx
+++ app/components/sensor-card.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
+import type { LucideIcon } from 'lucide-react'
 
-const sensorCard = () => {
+type SensorCardProps = {
+  Icon: LucideIcon
+  description: string
+  value: string
+}
+
+const SensorCard = ({Icon,description, value}: SensorCardProps) => {
   return (
-    <div></div>
+    <Card>
+      <CardHeader>
+
+        <CardTitle>
+          <Icon />
+        </CardTitle>
+        
+        <CardDescription>
+          {description}
+        </CardDescription>
+      </CardHeader>
+      
+      <CardContent className="grid gap-6">
+        {value}
+      </CardContent>
+      
+      <CardFooter>
+        progress bar
+      </CardFooter>
+    </Card>
   )
 }
 
-export default sensorCard
+export default SensorCard
--- app/routes/page/dashboard.tsx
+++ app/routes/page/dashboard.tsx
@@ -7,6 +7,7 @@ import OnlineStatus from '~/components/online-status';
 // import { PlaceholderPattern } from '~/components/placeholder-pattern';
 // import { Button, Input, Label } from '~/components/ui';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
+import SensorCard from '~/components/sensor-card';
 
 const Dashbooard = () => {
   return (
@@ -65,6 +66,7 @@ const Dashbooard = () => {
           <div className="grid auto-rows-min gap-4 sm:grid-cols-1">
 
             <TabsContent value="sensors">
+
               <Card>
                 <CardHeader>
                   <CardTitle className="flex items-center gap-2"> 
@@ -79,22 +81,9 @@ const Dashbooard = () => {
           
                   <div className="flex h-full flex-1 flex-col gap-0 rounded-xl overflow-x-auto">
                     <div className="grid auto-rows-min gap-2 sm:grid-cols-5">
-                      <Card>
-                        <CardHeader>
-                          <CardTitle>
-                            <ThermometerSun />
-                          </CardTitle>
-                          <CardDescription>
-                            Temperature
-                          </CardDescription>
-                        </CardHeader>
-                        <CardContent className="grid gap-6">
-                            0.0 °C
-                        </CardContent>
-                        <CardFooter>
-                          progress bar
-                        </CardFooter>
-                      </Card>
+                      
+                      <SensorCard Icon={ThermometerSun} description='Temperature' value='39 °C' />
+
                     </div>
                   </div>
 
