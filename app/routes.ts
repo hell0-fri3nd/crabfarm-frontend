85f9ccd9a5e695e4f5bc605c8756eda7a6c1159a
Fsociety-Mrn <lisboamillen30@gmail.com>
2025-12-30 23:00:12 +0800

    fix: change the webpage from weigh and snap into weigh and scan

--- app/routes.ts
+++ app/routes.ts
@@ -12,7 +12,7 @@ export default [
     route("/page", "routes/page-layout.tsx", [
         route("dashboard", "routes/page/dashboard.tsx"),
         route("logs", "routes/page/logs.tsx"),
-        route("weigh-and-snap", "routes/page/weigh-and-snap.tsx"),
+        route("weigh-and-scan", "routes/page/weigh-and-scan.tsx"),
     ]),
     
     route("","routes/default-routing.tsx"),
