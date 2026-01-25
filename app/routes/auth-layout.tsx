5859f09b26401c05578a025beafc7ed18ceb5db2
Fsociety-Mrn <lisboamillen30@gmail.com>
2026-01-25 16:46:38 +0800

    fix: remove the unused functions declared

--- app/routes/auth-layout.tsx
+++ app/routes/auth-layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { Route } from '../+types/';
 import AuthSimpleLayout from '~/components/layout/auth/auth-simple-layout'
 import { Outlet } from 'react-router';
-import ProtectedRoutes , { DefaultRoutes } from '~/components/layout/protected-routes';
+import  { DefaultRoutes } from '~/components/layout/protected-routes';
 import Footer from '~/routes/footer';
 
 export function meta({}: Route.MetaArgs) {
