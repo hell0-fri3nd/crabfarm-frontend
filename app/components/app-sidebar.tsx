ca31d53c910d39ddbaec6659c2316aeb4ed5253d
Fsociety-Mrn <62102054+hell0-fri3nd@users.noreply.github.com>
2026-03-18 21:45:16 +0800

    feat: add page for configurations

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -23,6 +23,11 @@ const mainNavItems: NavItem[] = [
         icon: LayoutGrid,
     },
     {
+        title: 'Configuration',
+        href: '/page/configuration',
+        icon: ClipboardClock,
+    },
+    {
         title: 'Logs',
         href: '/page/logs',
         icon: Logs,
@@ -36,11 +41,6 @@ const mainNavItems: NavItem[] = [
 
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Controller`s Shedule',
-        href: '/settings/controllers-shedule',
-        icon: ClipboardClock,
-    },
     {
         title: 'User Management',
         href: '/settings/users-settings',
