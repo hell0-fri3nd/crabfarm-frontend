0df27409218522b4e5512e28a34a36074c60e3c1
Fsociety-Mrn <62102054+hell0-fri3nd@users.noreply.github.com>
2026-04-14 22:41:57 +0800

    fix: ehnace the components props for switch label, by adding a ClassName

--- app/components/switch-label.tsx
+++ app/components/switch-label.tsx
@@ -1,16 +1,18 @@
 import {  Label } from '~/components/ui';
 import { Switch } from '~/components/ui/switch';
+import { cn } from '~/lib/utils';
 
 type SwitchLabelProps = {
   id: string
   label: string
   checked?: boolean
   onCheckedChange?: (checked: boolean) => void
+  className?: string
 }
 
-const SwitchLabel = ({ id, label, checked, onCheckedChange }: SwitchLabelProps) => {
+const SwitchLabel = ({ id, label, checked, onCheckedChange, className }: SwitchLabelProps) => {
     return (
-        <div className="flex items-center justify-between p-4 rounded-lg border border-border/30 hover:border-border/60 transition-colors bg-card/50">
+        <div className={cn("flex items-center justify-between", className)}>
             <Label htmlFor={id}> {label} </Label>
             <Switch id={id} checked={checked} onCheckedChange={onCheckedChange} />
         </div>
